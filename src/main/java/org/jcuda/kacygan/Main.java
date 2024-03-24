39f036cd75813dd16d49d2ed750295d012db614f
Decane <nightcyberhunter@gmail.com>
2024-03-25 00:15:11 +0100

    init project - installation of logger and jcuda from maven

---
+++ src/main/java/org/jcuda/kacygan/Main.java
@@ -0,0 +1,14 @@
+package org.jcuda.kacygan;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+
+        logger.info("Hello world!");
+    }
+}
